d90d07cc9fbeb3a92a94fdb0ab3d9525598d2cd7
usuariogenerico123 <negritolinuxero@gmail.com>
2026-02-10 18:51:31 +0000

    a

--- hola.go
+++ hola.go
@@ -1,11 +1,23 @@
 package main
 import(
 	"fmt"
+	"net/http"
 )
 
 
 func main(){
-	fmt.Println("HOLA MUNDOOOOO")
+	fmt.Println("iniciando script")
 
+
+	resp := http.FileServer(http.Dir("/"))
+
+	http.Handle("/", resp)
+
+	fmt.Println("iNICIANDO SERVER")
+	err := http.ListenAndServe(":3005", nil)
+	if(err != nil){
+		fmt.Println(err)
+	}
+	
 
 }
